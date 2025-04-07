9f047c3ca04fa16ef8745fdee1d2ca068e1d6552
blessed0314 <christian.daniel999@gmail.com>
2025-04-06 19:03:02 -0500

    fix loading modal

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,51 @@
 <script setup lang="ts">
-  import { computed, onMounted } from "vue";
-  import { useStockStore } from "./store/store-stock-selected";
+import { computed, onMounted } from "vue";
+import { useStockStore } from "./store/store-stock-selected";
 
-  import PageHeader from "./components/PageHeader.vue";
-  import StockList from "./components/StockList.vue";
-  import Pager from "./components/Pager.vue";
-  import type { Pagination } from './models/pagination.model'; 
-  const stockStore = useStockStore();
+import PageHeader from "./components/PageHeader.vue";
+import StockList from "./components/StockList.vue";
+import Pager from "./components/Pager.vue";
+import type { Pagination } from "./models/pagination.model";
+import LoadingModal from "./components/LoadingModal.vue";
 
-  const pagination = computed<Pagination>(() => ({
-    total: stockStore.total,
-    page: stockStore.page,
-    pageSize: stockStore.pageSize,
-  }));
+const stockStore = useStockStore();
 
-  onMounted(() => {
-    stockStore.isRecomendation = false;
-    stockStore.fetchStocks();
-  });
+const pagination = computed<Pagination>(() => ({
+  total: stockStore.total,
+  page: stockStore.page,
+  pageSize: stockStore.pageSize,
+}));
 
+onMounted(() => {
+  stockStore.isRecomendation = false;
+  stockStore.fetchStocks();
+});
 </script>
 
 <template>
   <PageHeader />
-  <div class="flex justify-center">
-    <button class="px-3 py-2 bg-sky-800/70 border rounded hover:bg-green-600/70" @click="stockStore.toggleRecomendation">
+  <div class="flex justify-center gap-4">
+    <button
+      class="w-70 px-3 py-2 bg-sky-800/70 border rounded hover:bg-green-600/70"
+      @click="stockStore.toggleRecomendation"
+    >
       <span class="font-bold text-white">
-        {{stockStore.isRecomendation ? "Return to Stockrating" :"Get Recomendations per Score"}}
+        {{
+          stockStore.isRecomendation
+            ? "Return to Stockrating"
+            : "Get Recomendations per Score"
+        }}
       </span>
     </button>
+    <button
+      v-if="!stockStore.isRecomendation"
+      class="w-70 px-3 py-2 bg-sky-800/70 border rounded hover:bg-green-600/70"
+      @click="stockStore.updateStocks"
+    >
+      <span class="font-bold text-white"> Update stock list </span>
+    </button>
   </div>
-  <Pager 
-    :pagination="pagination"
-    @pageChange="stockStore.onPageChange" 
-  />
+  <Pager :pagination="pagination" @pageChange="stockStore.onPageChange" />
   <StockList :stocks="stockStore.stocks" />
+  <LoadingModal v-if="stockStore.isUpdate" />
 </template>
---
+++ client/src/components/LoadingModal.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import loaderGif from '@/assets/loader.gif'
+</script>
+
+<template>
+  <div class="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+    <div
+      class="flex flex-col items-center justify-center bg-white p-4 rounded shadow-lg"
+    >
+      <img :src="loaderGif" alt="Loading" class="w-16 h-16" />
+      <p class="mt-2 text-lg font-semibold text-gray-700">Loading...</p>
+    </div>
+  </div>
+</template>
